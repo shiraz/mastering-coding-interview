50dfac45cb6905b9bda53d31cb569add03eabbb4
shiraz <mirzasbaig@nyu.edu>
2023-10-25 13:45:21 -0400

    Completed doubly linked list implementation

--- section8/LinkedList/main.js
+++ section8/LinkedList/main.js
@@ -127,7 +127,7 @@ class LinkedList {
   }
 }
 
-const myLinkedList = new LinkedList(10);
+const myLinkedList = new DoublyLinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
